c6a23774325c8ab3cdfbe5b1ee9b1983bcb76002
David Allen-Jordan <davidallenjordan@gmail.com>
2020-08-10 16:41:51 -0400

    Updates

---
+++ precache-manifest.6767127ef1e9073eacc4ca2c6482d24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e614553c7a1679bafc4ab8c783545c93",
+    "url": "/hushNoteProjectFive.git/index.html"
+  },
+  {
+    "revision": "ff36c85cbae3246458f1",
+    "url": "/hushNoteProjectFive.git/static/css/main.db15409e.chunk.css"
+  },
+  {
+    "revision": "ae5b60d00e2df6bb3cdd",
+    "url": "/hushNoteProjectFive.git/static/js/2.8398bfe8.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/hushNoteProjectFive.git/static/js/2.8398bfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff36c85cbae3246458f1",
+    "url": "/hushNoteProjectFive.git/static/js/main.78795d37.chunk.js"
+  },
+  {
+    "revision": "c0a206c3eaaf5916b998",
+    "url": "/hushNoteProjectFive.git/static/js/runtime-main.ddfe8c6b.js"
+  }
+]);
